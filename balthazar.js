1129119eb7d8f17d9f5e47fb3f6a32e66ff9910e
Lucas Fernandes <lucas.fernandes.94.lf@gmail.com>
2023-02-23 22:12:11 -0300

    Update
    Criação do placeHolder, estilização e JS para apresentar a pilha salva no local storage.
    
    Ajuste do balthazar.js

--- balthazar.js
+++ balthazar.js
@@ -1,56 +1,44 @@
-
-const form = document.querySelector('form');
+const form = document.querySelector("form");
 
 const pilha = [];
 var id = 0;
 
-form.addEventListener('submit', (event) => {
+form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  const inputNome = document.getElementById('inputNome');
-  const inputSetor = document.getElementById('inputSetor');
-  const inputEquipamento = document.getElementById('inputEquipamento');
-  const selectAtividade = document.querySelector('#atividade');
-  const inputDescricao = document.getElementById('inputDescricao');
+  const inputNome = document.getElementById("inputNome");
+  const inputSetor = document.getElementById("inputSetor");
+  const inputEquipamento = document.getElementById("inputEquipamento");
+  const selectAtividade = document.querySelector("#atividade");
+  const inputDescricao = document.getElementById("inputDescricao");
 
   const options = {
-    day: 'numeric',
-    month: 'numeric',
-    year: 'numeric',
-    hour: 'numeric',
-    minute: 'numeric',
+    day: "numeric",
+    month: "numeric",
+    year: "numeric",
+    hour: "numeric",
+    minute: "numeric",
     hour12: false,
   };
 
-
-    const ordemServico = {
-    
-    id: ++id.toString(),  
+  const ordemServico = {
+    id: id++,
     dataAbertura: new Date().toLocaleDateString("PT-br", options),
     cliente: inputNome.value,
     setor: inputSetor.value,
     equipamento: inputEquipamento.value,
     atividade: selectAtividade.value,
     descricao: inputDescricao.value,
+  };
 
-
-  }
-
-  //console.log(ordemServico)
+  ordemServico.id.toString();
 
   form.reset();
-
   pilha.push(ordemServico);
 
-  localStorage.setItem('ordemServico', JSON.stringify(pilha));
-
-  
-
-})
-
-
-const ordemServicoSalva = JSON.parse(localStorage.getItem('ordemServico'));
-  console.table(ordemServicoSalva);
+  localStorage.setItem("ordemServico", JSON.stringify(pilha));
+});
 
 // recuperando o objeto de LocalStorage
-
+const ordemServicoSalva = JSON.parse(localStorage.getItem("ordemServico"));
+console.table(ordemServicoSalva);
---
+++ populaTabela.js
@@ -0,0 +1,24 @@
+function popularTabela() {
+  const ordemServicoSalva = JSON.parse(localStorage.getItem("ordemServico"));
+  const tbody = document.querySelector(".content table tbody");
+  tbody.innerHTML = "";
+
+  for (let i = 0; i < ordemServicoSalva.length; i++) {
+    const ordemServico = ordemServicoSalva[i];
+
+    const row = document.createElement("tr");
+    row.innerHTML = `
+      <td class="idOS">${ordemServico.id}</td>
+      <td class="dateOpen">${ordemServico.dataAbertura}</td>
+      <td class="name">${ordemServico.cliente}</td>
+      <td class="sector">${ordemServico.setor}</td>
+      <td class="equip">${ordemServico.equipamento}</td>
+      <td class="active">${ordemServico.atividade}</td>
+      <td class="description">${ordemServico.descricao}</td>
+    `;
+
+    tbody.appendChild(row);
+  }
+}
+
+popularTabela();
